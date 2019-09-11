1277057a28cf0630cd29a259c4655bde43a3d626
Juliano Krindges <juliano9krindges@hotmail.com>
2019-09-10 23:52:10 -0300

    feat: consuming api github

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,16 @@
-import React from 'react'
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Main from './pages/Main/index'
-import Repository from './pages/Repository/index'
-
+import Main from './pages/Main/index';
+import Repository from './pages/Repository/index';
 
 export default function Routes() {
-  return(
+  return (
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Main} />
-        <Route path="/repository" component={Repository} />
+        <Route path="/repository/:repository" component={Repository} />
       </Switch>
     </BrowserRouter>
-  )
+  );
 }
